44ef2ca010764e74530a868a34966fd4386f0225
Jandir Moreira <jandirweb@gmail.com>
2021-10-21 02:24:20 -0300

    Fazendo modal exibir as paginas específicas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
                 </td>
                 <td class="participantes">{{ $projeto->participantes }}</td>
                 <td>
-                    <button type="button" class="btn btn-info roi-btn" data-toggle="modal" data-target="#modal-roi"><ion-icon name="cash-outline"></ion-icon> Simular ROI</button>
+                    <button type="button" class="btn btn-info roi-btn" data-toggle="modal" data-target="#modal-roi{{ $projeto->id }}"><ion-icon name="cash-outline"></ion-icon> Simular ROI</button>
                     <a href="/projetos/{{ $projeto->id }}" class="btn btn-info edit-btn"><ion-icon name="create"></ion-icon> Editar</a>
                     <form action="/projetos/{{ $projeto->id }}" method="POST" class="bt-deletar">
                         @csrf
@@ -53,11 +53,92 @@
 
                     @include('layouts.modal-delete')
 
-                    @include('layouts.modal-roi')
+                    
 
                 </td>
                 
             </tr>
+
+
+
+
+            <div class="modal fade" id="modal-roi{{ $projeto->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Simular ROI para o projeto: {{ $projeto->name }}</h5>
+                    </div>
+                    <div class="modal-body">
+                          <div class="col-md-12 mb-4">
+                              <div class="col-md-6">
+                                  <strong>Valor do projeto:</strong> R$ {{ $projeto->valor }}
+                              </div>
+                              <div class="col-md-6">
+                                  <strong>Risco:</strong>
+                                  @if ($projeto->risco == 0)
+                                  <span>Baixo</span>
+                                  @elseif($projeto->risco == 1)
+                                  <span>Médio</span>
+                                  @else
+                                  <span>Alto</span>
+                                  @endif
+                              </div>
+                          </div>
+                      <form>
+                        <div class="form-group">
+                          <div class="input-group mb-2">
+                              <div class="input-group-prepend">
+                                <div class="input-group-text">R$</div>
+                              </div>
+                              <input type="text" class="form-control" id="valor-invest" placeholder="Informe o valor a ser investido">
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <label for="message-text" class="col-form-label">Retorno do Investimento:</label>
+                          <textarea class="form-control" id="message-text" readonly></textarea>
+                        </div>
+                      </form>
+                    </div>
+                    <script>
+                      
+                      function simular($risco){
+                          $("#simular").on("click", function(){
+                              let name = document.querySelector("#valor-invest");
+                              let invest = name.value;
+                              let risco = $risco;
+                              console.log(invest)
+                              
+                              if($risco == 0){
+                              $roi = 0.05 * invest
+                              console.log($roi)
+                              document.getElementById('message-text').value = $roi
+                          }else if($risco == 1){
+                              $roi = 0.1 * invest
+                              console.log($roi)
+                              document.getElementById('message-text').value = $roi
+                          }else if($risco == 2){
+                              $roi = 0.2 * invest
+                              console.log($roi)
+                              document.getElementById('message-text').value = $roi
+                          }
+                          })
+                      }        
+              
+                   </script>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                      <button id="simular" type="button" class="btn btn-primary" onclick="simular({{ $projeto->risco }} );">Simular</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+
+
+
+
+
              @endforeach
 
         </tbody>
